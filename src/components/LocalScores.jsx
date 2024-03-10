7ed49308afc851fc3c42241815a7ed140c772d99
unknown <docbogdanc@yahoo.com>
2024-03-10 20:50:36 +0000

    some minimal css added

--- src/components/LocalScores.jsx
+++ src/components/LocalScores.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styleComponents.css";
+import Hero from "./Hero";
 
 function LocalScores() {
   return (
+    <div>
+        <Hero>
+            <h1>Title here</h1>
+         </Hero>
+    <h1>Welcome !</h1>
+    <p>text here</p>
     <div className="localScores">
-      <span>Local Scores here</span>
+      <h3>Local Scores here</h3>
     </div>
+  </div>
+
   );
 }
 
--- src/components/TopScores.jsx
+++ src/components/TopScores.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./styleComponents.css";
+import Hero from "./Hero";
 
 function TopScores() {
-  return (
-    <div className="topscores">
-      <span>TopScores here</span>
-    </div>
-  );
-}
+    return (
+        <div>
+            <Hero>
+                <h1>Title here</h1>
+             </Hero>
+        <h1>Welcome !</h1>
+        <p>text here</p>
+        <div className="topscores">
+          <h3>TopScores here</h3>
+        </div>
+      </div>
+    
+      );
+    }
+    
 
 export default TopScores;
+

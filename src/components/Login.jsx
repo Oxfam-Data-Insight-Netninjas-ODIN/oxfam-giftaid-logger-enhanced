e96bb6e049157607767ff52d68767968f1605268
Alex-Quayle <151681817+Alex-Quayle@users.noreply.github.com>
2024-03-15 21:30:03 +0000

    Installed Firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,28 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import "./CustomModal.css"; // Import custom CSS file for modal styling
-// import { initializeApp } from "firebase/app";
-// import "firebase/database";
-// import { getDatabase, ref, set, child, get } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCFPnbo7r2xIKUqW0I-2vqQS8Cz9G9M_tA",
-  authDomain: "oxfamlogger.firebaseapp.com",
-  databaseURL:
-    "https://oxfamlogger-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "oxfamlogger",
-  storageBucket: "oxfamlogger.appspot.com",
-  messagingSenderId: "142768490360",
-  appId: "1:142768490360:web:24e86a272d49e2ded82a8d",
-  measurementId: "G-KY1EJ33Z6G",
-};
-// const app = initializeApp(firebaseConfig);
-// const database = getDatabase(app);
-// // console.log(app);
-// // console.log(database);
-
 
 function Login() {
   const [username, setUsername] = useState("");
   const [pin, setPin] = useState("");
-  const [selectedArea, setSelectedArea] = useState("");
   const [showModal, setShowModal] = useState(true);
+
   const handleAccess = () => {
-    if (!username || !pin || !selectedArea) {
-      alert("Please enter username, pin, and select an area");
+    if (!username || !pin) {
+      alert("Please enter username and pin");
       return;
     }
-    // NEW CODE to retrieve the user and password from server and check if are same with input
-    const dbRef = ref(getDatabase());
-    get(child(dbRef, `users/`+ username +"/pass"))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          const retrievedPassword = snapshot.val().password;
 
+    if (username === "Test" && pin === "1234") {
+      console.log("Admin logged in:", { username, pin });
+    } else {
+      console.log("Logging in:", { username, pin });
+    }
 
-          if (retrievedPassword === pin) {
-            
-            console.log("Admin logged in:", { username, pin, selectedArea });
-            setShowModal(false);
-            window.location.href = "/home"; // Redirect to home page
-          } else {
-            console.log("Passwords do not match");
-            console.log("Logging in:", { username, pin, selectedArea });
-          }
-        } else {
-          console.log("No data available");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
+    setShowModal(false);
   };
 
   return (
-    <Modal show={showModal} backdrop="static" className="custom-modal">
+    <Modal show={showModal} backdrop="static">
       <Modal.Header closeButton>
         <Modal.Title>Login</Modal.Title>
       </Modal.Header>
@@ -70,7 +32,7 @@ function Login() {
             <label htmlFor="username">Username:</label>
             <input
               type="text"
-              className="form-control custom-input"
+              className="form-control"
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -81,33 +43,14 @@ function Login() {
             <label htmlFor="pin">Pin Number:</label>
             <input
               type="password"
-              className="form-control custom-input"
+              className="form-control"
               id="pin"
               value={pin}
               onChange={(e) => setPin(e.target.value)}
               required
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="area">Area:</label>
-            <select
-              id="area"
-              className="form-control custom-input"
-              value={selectedArea}
-              onChange={(e) => setSelectedArea(e.target.value)}
-              required
-            >
-              <option value="">Select an Area</option>
-              <option value="Yorkshire">Yorkshire</option>
-              <option value="Derbyshire">Derbyshire</option>
-              <option value="Shropshire">Shropshire</option>
-              <option value="Nottingham">Nottingham</option>
-              <option value="Wolverhampton">Wolverhampton</option>
-            </select>
-          </div>
-          <div className="form-group mb-3"> {}
-            <Button variant="primary" type="submit">Access</Button>
-          </div>
+          <Button variant="primary" type="submit">Access</Button>
         </form>
       </Modal.Body>
     </Modal>

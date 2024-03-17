609018116831f3aecf6a9d4762d5dadb354c7a17
jspitfire <152102332+jspitfire@users.noreply.github.com>
2024-03-17 19:56:35 +0000

    added tour.jsx and linked with app.jsx using start button for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import StoreData from './components/StoreData'
 import Leaderboard from './components/Leaderboard'
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import Date from './components/Date';
 import Footer from './components/Footer';
 import Wrapper from './components/Wrapper';
 import Marquee from './components/Marquee';
 import Login from './components/Login';
 import Counter from './components/Counter';
 import AppDate from './components/AppDate';
+import { TourComponent, TourSteps } from './components/Tour';
 
-function App() {
+  function App() {
+    const [isTourOpen, setIsTourOpen] = useState(false);
+
   return (
     <Router>
     <div>
       <Navbar />
-      <Date />
+        <button onClick={() => setIsTourOpen(true)}>Start Tour</button>
+        <TourComponent steps={TourSteps} isOpen={isTourOpen} onRequestClose={() => setIsTourOpen(false)} />
       <Wrapper>
         <Routes>
           <Route path="/" element={<Login />} />

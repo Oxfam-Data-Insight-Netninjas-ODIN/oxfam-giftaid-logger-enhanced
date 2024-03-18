1254d567163441039e1d13566162d0723b172739
Cristian Bogdan <docbogdanc@yahoo.com>
2024-03-18 20:06:10 +0000

    server data now represented in leader board and arranged by percentage

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styleComponents.css";
 import GlobalUserData from './globalUserData.json'
+import { initializeApp } from "firebase/app";
+import "firebase/database";
+import { getDatabase, ref, set, child, get } from "firebase/database";
+import firebaseConfig from "./FirebaseConfig";
+import { writeUserData } from "./firebaseFunct.js";
+
+const app = initializeApp(firebaseConfig);
+const dbRef = ref(getDatabase());
+
+// Fetch and filter data from server
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    get(child(dbRef, `users/`)).then((snapshot) => {
+      let storageData = snapshot.val();
+      let historyData = [];
+      Object.keys(storageData).forEach((key) => {
+        const newObject = storageData[key];
+        Object.values(newObject).forEach((nestedValue) => {
+          historyData.unshift(nestedValue);
+        });
+      });
+      const filteredData = historyData.filter(
+        (obj) => !Object.keys(obj).includes("password")
+      );
+      // add procentage to database array
+      filteredData.forEach((item) => {
+        item.proc = Math.round((item.gAid + 100) / (item.gAid + item.noGAid));
+      });
+      // Sort filteredData in descending order based on the 'proc' key
+      filteredData.sort((a, b) => b.proc - a.proc);
+      
+      console.log(filteredData);
+      resolve(filteredData);
+    }).catch((error) => {
+      reject(error);
+    });
+
+  });
+};
+
+// Usage
+fetchData().then((filteredData) => {
+  console.log(filteredData); // Access filteredData here
+}).catch((error) => {
+  console.error(error);
+});
 
 function TopScores() {
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    fetchData()
+      .then((data) => {
+        setFilteredData(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Welcome !</h1>
@@ -21,13 +79,13 @@ function TopScores() {
             </tr>
           </thead>
           <tbody>
-            {GlobalUserData.map((item, index) => (
+          {filteredData.map((item, index) => (
               <tr key={index}>
-                <td>{item.userCode}</td>
-                <td>{item.firstName}</td>
-                <td>{item.giftAid}</td>
-                <td>{item.nogiftAid}</td>
-                <td>{item.percentage}%</td>
+                <td>{item.username}</td>
+                <td>{item.name}</td>
+                <td>{item.gAid}</td>
+                <td>{item.noGAid}</td>
+                <td>{item.proc}%</td>
                 <td>{item.date}</td>
               </tr>
             ))}

e2e31e62a66d3a253122c2a22bb740a7cce2b10a
Alex-Quayle <151681817+Alex-Quayle@users.noreply.github.com>
2024-03-19 14:10:48 +0000

    Added thumbnail and page title.

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./styleComponents.css";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 import GlobalUserData from './globalUserData.json'
 import { initializeApp } from "firebase/app";
 import "firebase/database";

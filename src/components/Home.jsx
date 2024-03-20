839d9e49529fa2abe43601523ce441c669e196ee
Demitri <=>
2024-03-20 20:47:35 +0000

    icon added
    added icon into box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import "firebase/database";
 import { getDatabase, ref, set, child, get } from "firebase/database";
 import firebaseConfig from "./FirebaseConfig";
 import { writeUserData } from "./firebaseFunct.js";
+import till from "../assets/tillicon.png"
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -126,25 +127,23 @@ function Home() {
         </div>
       </div>
       
-     {/* New section with three separate boxes */}
-<div className="new-section container with-border">
+      {/* New section with three separate boxes */}
+      <div className="new-section container with-border">
     <h2>New Section</h2>
     <div className="row">
         <div className="col-12 col-md-4">
             <div className="spread-box">
-                <div className="box">
-                    <img src="assets/logout.svg" alt="Description of image">
-                </div>
-            </div>
-        </div>
-        <div className="col-12 col-md-4">
-            <div className="spread-box">
                 <div className="box"></div>
             </div>
         </div>
         <div className="col-12 col-md-4">
             <div className="spread-box">
                 <div className="box"></div>
+            </div>
+        </div>
+        <div className="col-12 col-md-4">
+            <div className="spread-box">
+                <div className="box"> <img src={till} alt="Description of image"></img></div>
             </div>
         </div>
     </div>

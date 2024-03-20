908a650120b36d1cd93f82a2cedfc5f1aa0c214c
Demitri <=>
2024-03-20 22:11:36 +0000

    remove the > alex please
    thanks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ import { writeUserData } from "./firebaseFunct.js";
 import { TourComponent, TourSteps } from './Tour';
 import qmark from '../assets/qmark.svg';
 
+import giftaidproject from "../assets/giftaidproject.png";
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
@@ -163,8 +164,10 @@ function Home() {
           </div>
         </div>
       </div>
+      <div><img src={giftaidproject}></img></div>
       <Footer />
     </div>
+
   );
 }
 

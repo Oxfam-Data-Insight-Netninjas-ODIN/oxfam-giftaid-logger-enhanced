84b800f0bfa326c33f1f427862f2060e8c375e62
Demitri <=>
2024-03-20 21:04:29 +0000

    added image
    image to description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,12 @@ import "firebase/database";
 import { getDatabase, ref, set, child, get } from "firebase/database";
 import firebaseConfig from "./FirebaseConfig";
 import { writeUserData } from "./firebaseFunct.js";
-import till from "../assets/tillicon.png"
-
+import { Image } from "react-bootstrap";
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 function Home() {
-  // code here to retrieve user counter history from server for current date if exist
+  // code here to retrieve user counter history from server for curent date if exist
   // and save it to local storage
   const username = localStorage.getItem("username");
   const dbRef = ref(getDatabase());
@@ -24,7 +23,7 @@ function Home() {
 
   const [GiftAid, setGiftAid] = useState(0);
   const [noGiftAid, setNoGiftAid] = useState(0);
-  // ==============testing retrieve object ===============
+  // ==============testin retrieve object ===============
 
   // ++++++++++++++++++end of test area  +++++++++++++++++
 
@@ -91,8 +90,7 @@ function Home() {
       writeUserData(userId, username, GiftAid, noGiftAid, date);
     }
   };
-
-  // check to see if numbers are 0, if not calculate the percentage
+  // check to see if number are 0, if not calculate the percentage
   const percentage =
     GiftAid + noGiftAid !== 0 ? (GiftAid / (GiftAid + noGiftAid)) * 100 : 0;
   const roundPercentage = Math.round(percentage.toFixed(2));
@@ -126,34 +124,11 @@ function Home() {
           </div>
         </div>
       </div>
-      
-      {/* New section with three separate boxes */}
-      <div className="new-section container with-border">
-    <h2>New Section</h2>
-    <div className="row">
-        <div className="col-12 col-md-4">
-            <div className="spread-box">
-                <div className="box"></div>
-            </div>
-        </div>
-        <div className="col-12 col-md-4">
-            <div className="spread-box">
-                <div className="box"></div>
-            </div>
-        </div>
-        <div className="col-12 col-md-4">
-            <div className="spread-box">
-                <div className="box"> <img src={till} alt="Description of image"></img></div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-      
       <Footer />
     </div>
+    
   );
+
 }
 
 export default Home;

9e84ca4bcd7b47355ed803b633d21386bac68927
Cristian Bogdan <docbogdanc@yahoo.com>
2024-03-21 14:28:20 +0000

    click delay registering data resolved

--- src/components/firebaseFunct.js
+++ src/components/firebaseFunct.js
@@ -4,7 +4,7 @@ import "firebase/database";
 import { getDatabase, ref, set, child, get, remove } from "firebase/database";
 // import usersDataJson from './assets/UsersData.json';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD7ZE16YXh3C_qntK8rDOJXIeFznpGAKJ4",
   authDomain: "oxfamodin-28c56.firebaseapp.com",
   databaseURL: "https://oxfamodin-28c56-default-rtdb.europe-west1.firebasedatabase.app",
@@ -20,16 +20,30 @@ const database = getDatabase(app);
 const db = getDatabase();
 
 
-// function to add a user to databse
-export function writeUserData(sufix, name, password) {
+// function to add a user data to current date to databse
+export function writeUserData(userId, username, GiftAid, noGiftAid, date) {
+  set(ref(db, "users/" + userId + "/" + date), {
+    username : userId,
+    name: username,
+    gAid: GiftAid,
+    noGAid: noGiftAid,
+    date: date
+  });
+}
+
+
+// function to add a NEW user to databse
+export function writeNewUserData(sufix, name, password) {
   set(ref(db, "users/" + name + sufix + "/pass"), {
     password: password
   });
 }
 
+
+
 // function to delete a user
 export function deleteUser(user) {
-  remove(ref(db, "users/" +user))
+  remove(ref(db, `users/${addUserId}`));
 }
 
 
@@ -66,5 +80,5 @@ export function writeMultipleUsersData(username, password, name, gAid, noGAid, d
   }
   
 
-  return username; // Return the last username processed
+  // return username; // Return the last username processed
 }
